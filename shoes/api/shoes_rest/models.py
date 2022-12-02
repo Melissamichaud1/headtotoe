867cf29a9e8326bcc742aec527cb8cb36314dc2b
Joseph Pinkus-Lock <joelock3@hotmail.com>
2022-12-02 16:28:38 -0500

    made shoes models and views

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# class
+class BinVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    bin_number = models.PositiveSmallIntegerField()
+    bin_size = models.PositiveSmallIntegerField()
+
+
+
+class Shoe(models.Model):
+    manufacturer =models.CharField(max_length=50)
+    model_name = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    picture_url = models.URLField(null=True)
+
+    bin = models.ForeignKey(
+        "BinVO",
+        related_name="shoes",
+        on_delete=models.CASCADE,
+        )
--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,56 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import Shoe, BinVO
 # Create your views here.
+
+class BinVODetailEncoder(ModelEncoder):
+    model = BinVO
+    properties = ["closet_name", "bin_number", "bin_size", "import_href"]
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties = ["model_name"]
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "manufacturer",
+        "model_name",
+        "color",
+        "picture_url",
+    ]
+    encoders = {
+        "bin": BinVODetailEncoder(),
+    }
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request, bin_vo_id=None):
+    if request.method == "GET":
+        shoes = Shoe.objects.filter(bin=bin_vo_id)
+        return JsonResponse(
+            {"shoes": shoes},
+            encoder=ShoeListEncoder,
+            )
+    else:
+        content = json.loads(request.body)
+
+        # Get the Conference object and put it in the content dict
+        try:
+            bin_href = content["bin"]
+            bin = BinVO.objects.get(import_href=bin_href)
+            content["bin"] = bin
+        except BinVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid bin id"},
+                status=400,
+            )
+
+        shoe = Shoe.objects.create(**content)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeDetailEncoder,
+            safe=False,
+        )
