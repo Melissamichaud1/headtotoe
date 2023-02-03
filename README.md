87daba8dca6f50d91572030c7e4c094fe751a1a7
Melissa <melissamichaud0319@gmail.com>
2023-02-02 22:59:23 -0500

    Edits

--- README.md
+++ README.md
@@ -2,17 +2,22 @@
 
 Team:
 
-* Melissa Michaud - Hats microservice
-* Joe Lock - Shoes microservice
+- Melissa Michaud - Hats microservice
+- Joe Lock - Shoes microservice
 
-## Design
+# **Getting the App Started**
 
-## Shoes microservice
+---
 
-Explain your models and integration with the wardrobe
-microservice, here.
-
-## Hats microservice
-
-Explain your models and integration with the wardrobe
-microservice, here.
+1. Git clone into your local repository
+   `git clone <<repo>>`
+2. cd into it
+   `cd headtotoe`
+3. Create a volume and name it postgres-data
+   `docker volume create pgdata`
+4. Build the image
+   `docker compose build`
+5. Run the containers
+   `dokcer compose up`
+6. Open browser to http://localhost:3000 to make sure it's running (it may take a minute or two)
+7. Once it's up and running, you can begin inputting data. You can access everything via the dropdowns on the NavBar
--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -3,27 +3,22 @@ import { Link } from "react-router-dom";
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">WARDROBIFY!</h1>
+      <h1 className="display-5 fw-bold">HeadToToe Inventory</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-          Need to keep track of your shoes and hats? We have
-          the solution for you!
+          Need to keep track of your shoes and hats? We have the solution for
+          you!
         </p>
         <img src="https://i.pinimg.com/originals/33/7a/dc/337adc9e1c236aeb1ba26ae5a5ba8ba0.jpg"></img>
         <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-              <Link
-                to="/hats/new"
-                className="btn btn-primary btn-lg px-4 gap-3"
-              >
-                Add A Hat
-              </Link>
-              <Link
-                to="/shoes/new"
-                className="btn btn-primary btn-lg px-4 gap-3"
-              >
-                Add A Shoe
-              </Link>
-            </div>
+          &nbsp;&nbsp;&nbsp;
+          <Link to="/hats/new" className="btn btn-dark btn-lg px-4 gap-3">
+            Add A Hat
+          </Link>
+          <Link to="/shoes/new" className="btn btn-dark btn-lg px-4 gap-3">
+            Add A Shoe
+          </Link>
+        </div>
       </div>
     </div>
   );
